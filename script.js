c26c4296992d13b3e8196f993cf264be51d53445
kkkkorvin <pingvinaka@gmail.com>
2023-07-16 03:39:15 +0700

    Weight for weight

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // www.codewars.com/users/kkorvin
 
 // String incrementer
+console.log("-------------------------String incrementer-------------------");
 
 function incrementString(str) {
   const index = str.match(/(\d+$)/)?.index;
-  let number = str.match(/(\d+$)/)?.[0] ?? 0;
-  const numLength = number.length;
-  let newStr = str.slice(0, index) + `${++number}`.padStart(numLength, 0);
+  let num = str.match(/(\d+$)/)?.[0] ?? 0;
+  const numLength = num.length;
+  let newStr = str.slice(0, index) + `${++num}`.padStart(numLength, 0);
   return newStr;
 }
 
@@ -15,3 +16,44 @@ console.log(incrementString("fo99obar99"));
 console.log(incrementString("foobar000"));
 console.log(incrementString("foobar999"));
 console.log(incrementString("foobar1"));
+
+// Weight for weight (Quick Sort)
+console.log(
+  "-------------------------Weight for weight (Quick Sort)-------------------"
+);
+
+function orderWeight(str) {
+  let arr = str.split(" ");
+
+  const sort = function (list) {
+    if (list.length <= 1) {
+      return list;
+    }
+
+    const pivot = list[list.length - 1];
+    const pivotWeight = pivot.split("").reduce((a, b) => +a + +b, 0);
+    const leftList = [];
+    const rightList = [];
+
+    for (let i = 0; i < list.length - 1; i++) {
+      let elementWeight = list[i].split("").reduce((a, b) => +a + +b, 0);
+      if (elementWeight < pivotWeight) {
+        leftList.push(list[i]);
+      } else if (elementWeight == pivotWeight) {
+        list[i] < pivot ? leftList.push(list[i]) : rightList.push(list[i]);
+      } else {
+        rightList.push(list[i]);
+      }
+    }
+
+    return [...sort(leftList), pivot, ...sort(rightList)];
+  };
+
+  let sortedList = sort(arr).join(" ");
+
+  return sortedList;
+}
+
+console.log(orderWeight("103 123 4444 999 2000"));
+console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+console.log(orderWeight("2 27 14 52 31 96 73 47 22 6"));
