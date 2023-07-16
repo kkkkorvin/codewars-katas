76ba342008614347c99635b7c068cbd05ff026fb
kkkkorvin <pingvinaka@gmail.com>
2023-07-17 04:10:26 +0700

    First non-repeating character

--- script.js
+++ script.js
@@ -57,3 +57,12 @@ function orderWeight(str) {
 console.log(orderWeight("103 123 4444 999 2000"));
 console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
 console.log(orderWeight("2 27 14 52 31 96 73 47 22 6"));
+
+// First non-repeating character
+console.log("-------------------First non-repeating character----------------");
+
+function firstNonRepeatingLetter(s) {
+  // Add your code here
+}
+
+console.log(firstNonRepeatingLetter("stress"));
