d16984ad5e6e02a6369ff0fc483d3c373f9a2fd7
kkkkorvin <pingvinaka@gmail.com>
2023-07-24 15:33:14 +0700

    PaginationHelper

--- script.js
+++ script.js
@@ -119,6 +119,7 @@ console.log(
 );
 
 // Greed is Good
+console.log("-------------------Greed is Good----------------");
 
 function score(dice) {
   console.log(dice);
@@ -163,4 +164,58 @@ function randomDice() {
   ];
 }
 
-console.log(score(randomDice()));
+// console.log(score(randomDice()));
+
+// PaginationHelper
+console.log("-------------------PaginationHelper----------------");
+
+class PaginationHelper {
+  constructor(collection, itemsPerPage) {
+    // The constructor takes in an array of items and a integer indicating how many
+    // items fit within a single page
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
+  }
+  itemCount() {
+    // returns the number of items within the entire collection
+    return this.collection.length;
+  }
+  pageCount() {
+    // returns the number of pages
+    return Math.ceil(this.collection.length / this.itemsPerPage);
+  }
+  pageItemCount(pageIndex) {
+    // returns the number of items on the current page. page_index is zero based.
+    // this method should return -1 for pageIndex values that are out of
+    if (pageIndex >= this.pageCount() || pageIndex < 0) return -1;
+    if (this.itemCount() / (pageIndex + 1) >= this.itemsPerPage)
+      return this.itemsPerPage;
+    return this.itemCount() % this.itemsPerPage;
+  }
+  pageIndex(itemIndex) {
+    // determines what page an item is on. Zero based indexes
+    // this method should return -1 for itemIndex values that are out of range
+    if (itemIndex < 0 || itemIndex >= this.collection.length) return -1;
+    return Math.floor(itemIndex / this.itemsPerPage);
+  }
+}
+
+let helper = new PaginationHelper(
+  [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24,
+  ],
+  10
+);
+
+console.log(helper.pageCount()); // should == 3
+console.log(helper.itemCount()); // should == 24
+console.log(helper.pageItemCount(0)); // should == 10
+console.log(helper.pageItemCount(1)); // last page - should == 10
+console.log(helper.pageItemCount(2)); // should == 4
+
+// pageIndex takes an item index and returns the page that it belongs on
+console.log(helper.pageIndex(5)); // should == 0 (zero based index)
+console.log(helper.pageIndex(2)); // should == 0
+console.log(helper.pageIndex(20)); // should == 2
+console.log(helper.pageIndex(-10)); // should == -1
